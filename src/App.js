8dc48eff204bf12d3a5fd065ba4466b181068e34
Paul Caruana <paul.caruana@leighton.com>
2020-12-08 17:09:14 +0000

    end of unit commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      // <BrowserRouter basename="/my_app"> this is the format if you are using a subdomain for the app e.g. paulcaruana.com/my_app 
+      <BrowserRouter> 
       <div className="App">
         <Blog />
       </div>
